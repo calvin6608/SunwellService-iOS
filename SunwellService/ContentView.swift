e2c6da4eac3107448802604682e228d2f7275d0a
calvin <calvin6608@gmail.com>
2026-07-02 18:47:20 +0800

    Initial iOS project

---
+++ SunwellService/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SunwellService
+//
+//  Created by Calvin on 2026/7/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SunwellService/SunwellServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunwellServiceApp.swift
+//  SunwellService
+//
+//  Created by Calvin on 2026/7/2.
+//
+
+import SwiftUI
+
+@main
+struct SunwellServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
